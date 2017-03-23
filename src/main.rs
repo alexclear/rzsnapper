b9c4033ca96dcf88dbe4fca7f8b7f0229b8a3c69
Alex Chistyakov <alexclear@gmail.com>
2017-03-23 11:13:10 +0300

    Add a command line option for a config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rzsnapper")
+        .author("Alex Chistyakov <alexclear@gmail.com>")
+        .arg(Arg::with_name("config")
+                 .short("c")
+                 .long("config")
+                 .value_name("FILE")
+                 .help("Sets a custom config file")
+                 .takes_value(true))
+        .arg(Arg::with_name("v").short("v").help("Enables verbose output").takes_value(false))
+        .get_matches();
+
+    let config = matches.value_of("config").unwrap_or("/opt/local/zsnapper/etc/zsnapper.yml");
+    println!("Value for config: {}", config);
 }
