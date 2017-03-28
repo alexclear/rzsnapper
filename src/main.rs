268c115e9cf5f27c19e13181be56ad883492f2cf
Alex Chistyakov <alexclear@gmail.com>
2017-03-28 22:11:27 +0300

    Read config from YAML

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 extern crate clap;
+extern crate yaml_rust;
 use clap::{Arg, App};
+use yaml_rust::YamlLoader;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
 
 fn main() {
     let matches = App::new("rzsnapper")
@@ -13,6 +18,13 @@ fn main() {
         .arg(Arg::with_name("v").short("v").help("Enables verbose output").takes_value(false))
         .get_matches();
 
-    let config = matches.value_of("config").unwrap_or("/opt/local/zsnapper/etc/zsnapper.yml");
-    println!("Value for config: {}", config);
+    let config_name = matches.value_of("config").unwrap_or("/opt/local/zsnapper/etc/zsnapper.yml");
+    println!("Value for config: {}", config_name);
+
+    let file = File::open(config_name).unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).unwrap();
+
+    let config = YamlLoader::load_from_str(contents.as_str()).unwrap();
 }
